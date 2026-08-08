5869394e9c20f1636382ab280de95cfd2de94c3e
Max Suarez <max.san.suarez.o@gmail.com>
2026-08-07 22:41:39 -0400

    add error handler

--- server.py
+++ server.py
@@ -11,6 +11,10 @@ def render_index_page():
 def detect_emotion():
     text = request.args.get('textToAnalyze')
     emotions = emotion_detector(text)
+
+    if not emotions:
+        return f"For the given statement, the system response is 'anger': None, 'disgust': None, 'fear': None, 'joy': None and 'sadness': None. The dominant emotion is None.", 400
+
     return f"For the given statement, the system response is 'anger': {emotions['anger']}, 'disgust': {emotions['disgust']}, 'fear': {emotions['fear']}, 'joy': {emotions['joy']} and 'sadness': {emotions['sadness']}. The dominant emotion is {emotions['dominant_emotion']}."
 
 if __name__ == "__main__":
