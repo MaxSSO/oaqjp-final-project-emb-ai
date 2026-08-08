932904cd913a5100e1056752ef7bb8a29cc9778f
Max Suarez <max.san.suarez.o@gmail.com>
2026-08-07 21:25:34 -0400

    add format

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,25 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = { "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock" }
-    json = { "raw_document": { "text": text_to_analyze } }
-    obj = requests.post(url, json=json, headers=headers)
-    return obj.text
+    body = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json=body, headers=headers)
+    json_response = json.loads(response.text)
+
+    emotion = json_response['emotionPredictions'][0]['emotion']
+    max_score = 0
+    dominant_emotion = 'none'
+
+    for k in emotion.keys():
+        emotion_score = emotion[k]
+        if emotion_score > max_score:
+            dominant_emotion = k
+            max_score = emotion_score
+
+    emotion['dominant_emotion'] = dominant_emotion
+
+    return emotion
+
+print(emotion_detector('I love this new technology.'))
